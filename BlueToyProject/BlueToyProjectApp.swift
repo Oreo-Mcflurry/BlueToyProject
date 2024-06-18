0d3cd204cdd33932e0692c4e4b7419351ad961eb
유인호 <yuiop1029@naver.com>
2024-06-18 13:33:06 +0900

    Initial Commit

---
+++ BlueToyProject/BlueToyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueToyProjectApp.swift
+//  BlueToyProject
+//
+//  Created by A_Mcflurry on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct BlueToyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
