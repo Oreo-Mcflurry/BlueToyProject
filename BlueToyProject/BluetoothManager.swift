e39e9933ec5002f16b8d4d5a01c114fb03196c76
유인호 <yuiop1029@naver.com>
2024-06-18 15:23:43 +0900

    [ADD] Add BluetoothManager

---
+++ BlueToyProject/BluetoothManager.swift
@@ -0,0 +1,125 @@
+//
+//  BluetoothManager.swift
+//  BlueToyProject
+//
+//  Created by A_Mcflurry on 6/18/24.
+//
+
+import Foundation
+import CoreBluetooth
+
+final class BluetoothManager: NSObject, ObservableObject {
+    static let shared = BluetoothManager.init()
+    private override init() { 
+        super.init()
+        self.centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+    
+    @Published var discoveredPeripherals: [CBPeripheral] = []
+    @Published var isScanning = false {
+        didSet {
+            switch isScanning {
+            case true:
+                startScanning()
+            case false:
+                stopScanning()
+            }
+        }
+    }
+    
+    private var centralManager: CBCentralManager!
+    private var connectedPeripheral: CBPeripheral?
+}
+
+extension BluetoothManager: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .unknown:
+            print("unknown")
+        case .resetting:
+            print("resetting")
+        case .unsupported:
+            print("unsupported")
+        case .unauthorized:
+            print("unauthorized")
+        case .poweredOff:
+            print("poweredOff")
+        case .poweredOn:
+            print("poweredOn")
+        @unknown default:
+            print("unknown default")
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if !discoveredPeripherals.contains(where: { $0.identifier == peripheral.identifier }) {
+            discoveredPeripherals.append(peripheral)
+        }
+    }
+
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        connectedPeripheral = peripheral
+        peripheral.delegate = self
+        peripheral.discoverServices(nil)
+    }
+
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        print("Failed to connect to \(peripheral.name ?? "a peripheral"): \(error?.localizedDescription ?? "Unknown error")")
+    }
+
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        if connectedPeripheral == peripheral {
+            connectedPeripheral = nil
+        }
+    }
+    
+    private func startScanning() {
+        centralManager.scanForPeripherals(withServices: nil, options: nil)
+    }
+
+    private func stopScanning() {
+        centralManager.stopScan()
+    }
+}
+
+extension BluetoothManager: CBPeripheralDelegate {
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        if let services = peripheral.services {
+            for service in services {
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
+        }
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        if let characteristics = service.characteristics {
+            for characteristic in characteristics {
+                peripheral.readValue(for: characteristic)
+                peripheral.setNotifyValue(true, for: characteristic)
+            }
+        }
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        if let value = characteristic.value {
+            print("Updated value for characteristic \(characteristic.uuid): \(value)")
+        }
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
+        if let error = error {
+            print("Error writing value for characteristic \(characteristic.uuid): \(error.localizedDescription)")
+        } else {
+            print("Successfully wrote value for characteristic \(characteristic.uuid)")
+        }
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didUpdateNotificationStateFor characteristic: CBCharacteristic, error: Error?) {
+        if let error = error {
+            print("Error updating notification state for characteristic \(characteristic.uuid): \(error.localizedDescription)")
+        } else {
+            print("Notification state updated for characteristic \(characteristic.uuid)")
+        }
+    }
+}
+
